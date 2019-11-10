b1b2cbeea3b2a3a86ad78d2de9e87f776ee82ac7
pcast086 <pcast086@fiu.edu>
2019-11-10 10:04:05 -0500

    Lessened time -cp

--- app/src/main/java/com/example/technica2019/content_load.java
+++ app/src/main/java/com/example/technica2019/content_load.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.os.Handler;
 
 public class content_load extends AppCompatActivity {
-private static int SPLASH_TIME_OUT = 5000;
+private static int SPLASH_TIME_OUT = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
