de412b8d0ec859b3540b2dbe948e6a7f6078bcc0
pcast086 <pcast086@fiu.edu>
2019-11-10 10:37:54 -0500

    CP and I fixed background images, toast notifications etc.

--- app/src/main/java/com/example/technica2019/content_assignments.java
+++ app/src/main/java/com/example/technica2019/content_assignments.java
@@ -62,23 +62,44 @@ public class content_assignments extends AppCompatActivity {
         questions.add("Due: November 14, 11 p.m.                        ETC: 2 hrs " +
                 "\nLaw of Gravity Paper" +
                 "\nPHY2048");
+        questions.add("Due: November 10, 10 a.m.                        ETC: 1 hr " +
+                "\nFuture of Technology Presentation" +
+                "\nENC 3249");
+        questions.add("Due: November 10, 2 p.m.                          ETC: 40 mins " +
+                "\nCalculus Assignment" +
+                "\nMAC 2312");
+        questions.add("Due: November 11, 11 p.m.                        ETC: 2 hrs " +
+                "\nFuture of Technology Reflection Essay" +
+                "\nENC 3249");
+        questions.add("Due: November 11, 8 a.m.                          ETC: 50 mins " +
+                "\nOpinion Paper" +
+                "\nIDH 3034");
+        questions.add("Due: November 11, 8 a.m.                          ETC: 40 mins " +
+                "\nCurrent Events Presentation" +
+                "\nIDH 3034");
+        questions.add("Due: November 11, 11 p.m.                        ETC: 35 mins " +
+                "\nArrays Pseudo Code" +
+                "\nCOP 2210");
+        questions.add("Due: November 12, 8 a.m.                          ETC: 1 hr " +
+                "\nChapters 1-5 Study Guide" +
+                "\nMAC 2312");
 
         ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, questions);
 
 
         listView.setAdapter(arrayAdapter);
 
-/*        listView.setOnClickListener(new AdapterView.OnItemClickListener() {
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view) {
-                Toast.makeText(, "Selected Task Now In Progress", Toast.LENGTH_SHORT).show();
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(), "Selected Task Now In Progress", Toast.LENGTH_SHORT).show();
                 Toast toast = Toast.makeText(getApplicationContext(), "Selected Task Now in Progress", Toast.LENGTH_SHORT);
                 toast.show();
 
             }
         });
-*/
+
     }
 
 
