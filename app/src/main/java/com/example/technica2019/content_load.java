3251cf9e011d58046b6624d3f7002d202ee28ce9
Chelsea Pierre <cpier114@fiu.edu>
2019-11-10 06:13:56 -0500

    Added a splash screen affect for loading screen, will transition to next page after timer ends

---
+++ app/src/main/java/com/example/technica2019/content_load.java
@@ -0,0 +1,28 @@
+package com.example.technica2019;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class content_load extends AppCompatActivity {
+private static int SPLASH_TIME_OUT = 10000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_content_load);
+
+        new Handler().postDelayed(new Runnable(){
+
+            @Override
+                    public void run (){
+                Intent intent = new Intent(content_load.this, content_assignments.class);
+                startActivity(intent);
+                finish();
+            }
+
+        }, SPLASH_TIME_OUT);
+    }
+}
